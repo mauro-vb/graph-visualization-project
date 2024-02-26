28f2a080516d49a15383f906187f8b1029f9f316
Mauro Vazquez Bassat <mvb.codes@gmail.com>
2024-02-26 17:03:39 +0100

    added axis and label options

--- app/st_app.py
+++ app/st_app.py
@@ -1,16 +1,14 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 from classes import Node, Graph, Tree
 import pygraphviz
-import numpy as np
 import os
 
 # Main Streamlit app
 def main():
     st.title("Graph and Tree Visualization App")
-    datasets = ["Datasets/" + dataset for dataset in os.listdir("Datasets") if dataset.endswith(".dot")]
+    datasets = ["Select"] + ["Datasets/" + dataset for dataset in os.listdir("Datasets") if dataset.endswith(".dot")]
     selected_file = st.selectbox('select a graph to visualize', datasets)
-    if selected_file is not None:
+    if selected_file != "Select":
         G = pygraphviz.AGraph(selected_file)
         visualization_options = ["Select", "Tree", "Graph"]
         visualization_type = st.selectbox("Choose Visualization Type", visualization_options)
@@ -26,21 +24,24 @@ def main():
                 graph.add_node(new_node)
 
             traversal_options = ["DFS", "BFS"]
+            labels = st.checkbox("labels")
             traversal_type = st.selectbox("Choose Tree Traversal Type", traversal_options)
             root_label = st.text_input("Enter the root label for your tree:","1")
 
             if traversal_type == "DFS":
                 graph.compute_dfs_tree(root_label)
-                fig = graph.root.draw_tree()
+                fig = graph.root.draw_tree(labels)
                 st.pyplot(fig=fig)
             elif traversal_type == "BFS":
                 graph.compute_bfs_tree(root_label)
-                fig = graph.root.draw_tree()
+                fig = graph.root.draw_tree(labels)
                 st.pyplot(fig=fig)
 
         elif visualization_type == "Graph":
             layout_options = ["Random", "Circular"]
             layout_type = st.selectbox("Choose Graph Layout Type", layout_options)
+            axis = st.checkbox("axis")
+            labels = st.checkbox("labels")
             graph = Graph()
 
             for node in G.nodes():
@@ -54,7 +55,7 @@ def main():
                 graph.generate_random_coordinates()
             elif layout_type == "Circular":
                 graph.generate_circular_coordinates()
-            fig = graph.plot_graph()
+            fig = graph.plot_graph(axis=axis,node_tag=labels)
             st.pyplot(fig=fig)
 
 if __name__ == "__main__":
